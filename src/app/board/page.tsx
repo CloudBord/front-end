2a41af2dc3ac1306eda9e0eab6b9cc9ab8a9eedf
Lex de Kort <I488027@fontysict.nl>
2024-04-24 23:59:32 +0200

    Add navbar and header components

---
+++ src/app/board/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+    return (
+      <div className="flex justify-center">
+        <div className="container">
+          Cool boards
+        </div>
+      </div>
+    );
+  }
+  
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,1 +1,32 @@
 import React from "react";
+import Link from "next/link";
+
+export const NavBar = () => {
+    return (
+        <nav className="flex justify-center items-center bg-[#00ff00] border-b">
+            <div className="container flex justify-between py-3">
+                <div className="logo">
+                    Logo
+                </div>
+                <div className="linkies">
+                    <ul className="flex flex-col ">
+                        <li>
+                            <Link href="/" className="sidebar-link px-2">
+                                Home
+                            </Link>
+                            <Link href="/board" className="sidebar-link px-2">
+                                Board
+                            </Link>
+                            <Link href="/" className="sidebar-link px-2">
+                                Home
+                            </Link>
+                            <Link href="/" className="sidebar-link ps-2">
+                                Login
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+};
