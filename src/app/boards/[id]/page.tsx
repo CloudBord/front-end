0fd2020cb33c95a9ba7ccb1862a278253cda564d
lexdekort <l.kort@beginstation.nl>
2024-04-25 16:46:15 +0200

    Add tldraw library

--- src/app/boards/[id]/page.tsx
+++ src/app/boards/[id]/page.tsx
@@ -1,10 +1,18 @@
+'use client'
+
 import React from "react";
+import { Tldraw } from "tldraw";
+import 'tldraw/tldraw.css';
 
 export default function Board() {
     return (
       <div className="flex justify-center">
         <div className="container">
-            My board
+          <main>
+            <div style={{ position: 'fixed', inset: 0 }}>
+              <Tldraw />
+            </div>
+          </main>
         </div>
       </div>
     );
