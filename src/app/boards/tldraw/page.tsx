13ec996894d6897dd2ad33a170a6e1c264e0e99e
lexdekort <l.kort@beginstation.nl>
2024-05-22 10:46:20 +0200

    Add save button

--- src/app/boards/tldraw/page.tsx
+++ src/app/boards/tldraw/page.tsx
@@ -7,13 +7,36 @@ import 'tldraw/tldraw.css';
 const SaveButton = () => {
   const editor = useEditor();
   return (
-    <button onClick={() => {
-      const snapshot = editor.store.getSnapshot();
-      const stringified = JSON.stringify(snapshot);
-      console.log(stringified);
-    }}>
+    // <button onClick={() => {
+    //   const snapshot = editor.store.getSnapshot();
+    //   const stringified = JSON.stringify(snapshot);
+    //   console.log(stringified);
+    // }}>
+    //   Save
+    // </button>
+
+    <button
+      style={{
+        position: 'absolute',
+        top: 10,
+        right: 165,
+        padding: '10px 20px',
+        backgroundColor: '#007BFF',
+        color: '#FFF',
+        border: 'none',
+        borderRadius: '5px',
+        cursor: 'pointer',
+        zIndex: 1000,
+      }}
+      onClick={() =>{
+        const snapshot = editor.store.getSnapshot();
+        const stringified = JSON.stringify(snapshot);
+        console.log(stringified);
+      }}
+    >
       Save
     </button>
+
   )
 }
 
